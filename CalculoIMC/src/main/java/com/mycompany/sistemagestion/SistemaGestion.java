441d3b603ba3b207ece093f9e937779439edad9e
Enrique García <107608830+enriquegarcia7@users.noreply.github.com>
2022-08-23 10:21:39 -0400

    IMC Commit

---
+++ CalculoIMC/src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sistemagestion;
+
+/**
+ *
+ * @author Enrique
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
